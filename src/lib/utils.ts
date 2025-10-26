be746f010ca7160c31e39d27ef7eea95acd4d101
Sadiq Salau <sadiqsalau888@gmail.com>
2025-10-26 11:30:48 +0100

    feat: Added utils (getWalletAddressFromPrivateKey and uuid)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,9 @@
 import clsx, { type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import copy from "copy-to-clipboard";
+import { Wallet } from "ethers/wallet";
+
+export { v4 as uuid } from "uuid";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(...inputs));
@@ -8,4 +11,14 @@ export function cn(...inputs: ClassValue[]) {
 
 export function copyToClipboard(content: string) {
   copy(content);
+}
+
+export function getWalletAddressFromPrivateKey(privateKey: string) {
+  if (!privateKey || privateKey.length === 0) return "";
+  try {
+    const wallet = new Wallet(privateKey);
+    return wallet.address;
+  } catch {
+    return "";
+  }
 }
