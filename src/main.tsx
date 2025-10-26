521e053325d258bb8b32f120161a277ee4c972bc
Sadiq Salau <sadiqsalau888@gmail.com>
2025-10-26 17:21:02 +0100

    feat: Register Service Worker

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,12 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
+import { registerSW } from "virtual:pwa-register";
 
+/** Register Service Worker */
+registerSW({ immediate: true });
+
+/** React Query Client */
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
