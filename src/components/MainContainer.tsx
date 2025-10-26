0f49dc14b47624bfcbf7b8711a8d37bb1c65d856
Sadiq Salau <sadiqsalau888@gmail.com>
2025-10-26 13:45:15 +0100

    feat: Allow passing className prop

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -1,7 +1,20 @@
-const MainContainer = ({ children }: { children: React.ReactNode }) => {
+import { cn } from "../lib/utils";
+
+const MainContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
   return (
     <main className="flex flex-col p-4 grow min-w-0 min-h-0">
-      <div className="w-full max-w-sm mx-auto flex flex-col grow min-w-0 min-h-0">
+      <div
+        className={cn(
+          "w-full max-w-sm mx-auto flex flex-col grow min-w-0 min-h-0",
+          className
+        )}
+      >
         {children}
       </div>
     </main>
