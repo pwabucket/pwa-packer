4143fb293b228a39592f7de682b67dfb755d98e0
Sadiq Salau <sadiqsalau888@gmail.com>
2025-10-26 12:33:17 +0100

    feat: Added Protected Routes Wrapper component

---
+++ src/routes/ProtectedRoutes.tsx
@@ -0,0 +1,9 @@
+import { Navigate, Outlet } from "react-router";
+import usePassword from "../hooks/usePassword";
+
+const ProtectedRoutes = () => {
+  const isAuthenticated = usePassword() !== null;
+  return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
+};
+
+export { ProtectedRoutes };
