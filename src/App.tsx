410ba31959a520c960087f3df9ed0bd9b58c0fe7
Sadiq Salau <sadiqsalau888@gmail.com>
2025-10-26 12:34:20 +0100

    feat: Registered Welcome, Dashboard and Account Creation Page routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Route, Routes } from "react-router";
+import { Welcome } from "./pages/Welcome";
+import { Dashboard } from "./pages/Dashboard";
+import { AccountCreationPage } from "./pages/AccountCreationPage";
+import { ProtectedRoutes } from "./routes/ProtectedRoutes";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route index element={<Welcome />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="accounts/new" element={<AccountCreationPage />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
