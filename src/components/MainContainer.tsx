9451ccf20cb1da2dddf01832f3f5eec9ee486ca7
Sadiq Salau <sadiqsalau888@gmail.com>
2025-10-26 13:42:39 +0100

    feat: Set wrapper to flex

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -1,7 +1,9 @@
 const MainContainer = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="grow min-w-0 min-h-0 flex flex-col p-4 w-full">
-      <div className="w-full max-w-sm mx-auto flex flex-col">{children}</div>
+    <main className="flex flex-col p-4 grow min-w-0 min-h-0">
+      <div className="w-full max-w-sm mx-auto flex flex-col grow min-w-0 min-h-0">
+        {children}
+      </div>
     </main>
   );
 };
