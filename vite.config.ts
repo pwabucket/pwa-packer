0a0dacb16dd1d45496812876eb4d94a8b6cb1657
Sadiq Salau <sadiqsalau888@gmail.com>
2025-10-26 17:17:17 +0100

    feat: Configured PWA Plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,65 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import process from "process";
+import version from "vite-plugin-package-version";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  /** Env */
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [
+      /** Plugins */
+      version(),
+      VitePWA({
+        registerType: "autoUpdate",
+        workbox: {
+          globPatterns: ["**/*.*"],
+          maximumFileSizeToCacheInBytes: 5 * 1024 ** 2,
+        },
+        manifest: {
+          name: "Packer",
+          short_name: "Packer",
+          description:
+            "Perform BSC Transactions in Batch with built-in Hash Maker.",
+          theme_color: "#0a0a0a",
+          background_color: "#0a0a0a",
+          icons: [
+            {
+              src: "pwa-64x64.png",
+              sizes: "64x64",
+              type: "image/png",
+            },
+            {
+              src: "pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+            {
+              src: "maskable-icon-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "maskable",
+            },
+          ],
+        },
+      }),
+      ViteEjsPlugin(env),
+      tailwindcss(),
+      react({
+        babel: {
+          plugins: [["babel-plugin-react-compiler"]],
+        },
+      }),
+    ],
+  };
 });
