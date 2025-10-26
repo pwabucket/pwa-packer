896dd525601454b9d48ef2413be72454dca90359
Sadiq Salau <sadiqsalau888@gmail.com>
2025-10-26 12:29:43 +0100

    feat: Added Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,18 @@
+import { cn } from "../lib/utils";
+
+const Button = (props: React.ComponentProps<"button">) => {
+  return (
+    <button
+      {...props}
+      className={cn(
+        "px-4 py-2 rounded-full cursor-pointer",
+        "bg-yellow-500 text-black",
+        "hover:bg-yellow-400",
+        "disabled:opacity-50 disabled:cursor-not-allowed",
+        props.className
+      )}
+    />
+  );
+};
+
+export { Button };
