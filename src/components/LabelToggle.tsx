b2ea0f374c202bf7cb5877430947409a92f20c2a
Sadiq Salau <sadiqsalau888@gmail.com>
2025-10-28 03:41:18 +0100

    feat: Added label toggle component

---
+++ src/components/LabelToggle.tsx
@@ -0,0 +1,29 @@
+import { cn } from "../lib/utils";
+import { memo } from "react";
+
+import { Toggle } from "./Toggle";
+
+interface LabelToggleProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  children: React.ReactNode;
+}
+
+const LabelToggle = memo(function LabelToggle({
+  children,
+  className,
+  ...props
+}: LabelToggleProps) {
+  return (
+    <label
+      className={cn(
+        "dark:bg-neutral-900",
+        "flex items-center gap-4 px-3 py-2 cursor-pointer rounded-full",
+        "has-[input:disabled]:opacity-60"
+      )}
+    >
+      <Toggle {...props} />
+      <h4 className={cn("min-w-0 min-h-0 grow flex", className)}>{children}</h4>
+    </label>
+  );
+});
+
+export { LabelToggle };
