9ad22b5210b0f4b450e07c479908ac5ce666ed38
Sadiq Salau <sadiqsalau888@gmail.com>
2025-11-06 06:38:14 +0100

    feat: Added close button

--- src/pages/SendResults.tsx
+++ src/pages/SendResults.tsx
@@ -3,7 +3,8 @@ import { PopupDialog } from "../components/PopupDialog";
 import { Dialog } from "radix-ui";
 import { Button } from "../components/Button";
 import { SendResultsAccordion } from "../components/SendResultsAccordion";
-import { MdReceiptLong } from "react-icons/md";
+import { MdOutlineClose, MdReceiptLong } from "react-icons/md";
+import { cn } from "../lib/utils";
 
 /** Send Results Props Interface */
 interface SendResultsProps {
@@ -14,9 +15,22 @@ interface SendResultsProps {
 const SendResults = ({ results }: SendResultsProps) => {
   return (
     <PopupDialog onInteractOutside={(e) => e.preventDefault()}>
-      <Dialog.Title className="text-xl text-center text-yellow-500">
-        <MdReceiptLong className="inline-block mr-2" /> Send Results
-      </Dialog.Title>
+      <div className="flex items-center gap-4">
+        <span className="size-10 shrink-0" />
+        <Dialog.Title className="grow min-w-0 font-bold text-yellow-500 text-center">
+          <MdReceiptLong className="inline-block mr-2" /> Send Results
+        </Dialog.Title>
+        <Dialog.Close
+          className={cn(
+            "size-10 shrink-0 flex items-center justify-center",
+            "rounded-full text-neutral-400",
+            "hover:bg-neutral-800 hover:text-red-500 cursor-pointer"
+          )}
+        >
+          <MdOutlineClose className="size-6" />
+        </Dialog.Close>
+      </div>
+
       <Dialog.Description className="text-neutral-400 text-sm text-center">
         Click each item to view detailed information about the send results.
       </Dialog.Description>
