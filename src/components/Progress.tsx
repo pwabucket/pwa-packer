e52c9defd9a1ec89ae09b430e392c96d15915e73
Sadiq Salau <sadiqsalau888@gmail.com>
2025-11-15 08:45:14 +0100

    feat: Display progress text

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -7,15 +7,20 @@ interface ProgressProps {
 
 const Progress = ({ current, max }: ProgressProps) => {
   return (
-    <ProgressPrimitive.Root
-      value={current}
-      className="w-full h-2 overflow-hidden bg-neutral-800 border border-neutral-700 rounded-full"
-    >
-      <ProgressPrimitive.Indicator
-        className="bg-yellow-500 h-full transition-all duration-500"
-        style={{ width: `${(current / max) * 100}%` }}
-      />
-    </ProgressPrimitive.Root>
+    <div className="flex flex-col w-full gap-1">
+      <ProgressPrimitive.Root
+        value={current}
+        className="w-full h-2 overflow-hidden bg-neutral-800 border border-neutral-700 rounded-full"
+      >
+        <ProgressPrimitive.Indicator
+          className="bg-yellow-500 h-full transition-all duration-500"
+          style={{ width: `${(current / max) * 100}%` }}
+        />
+      </ProgressPrimitive.Root>
+      <p className="text-xs text-center text-yellow-500">
+        {current} / {max}
+      </p>
+    </div>
   );
 };
 
