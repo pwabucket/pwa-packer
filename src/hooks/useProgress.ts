83aef9078238568007c834754eb1e0773c08baa9
Sadiq Salau <sadiqsalau888@gmail.com>
2025-11-15 14:42:55 +0100

    feat: Set target to 0

--- src/hooks/useProgress.ts
+++ src/hooks/useProgress.ts
@@ -1,12 +1,12 @@
 import { useCallback, useState } from "react";
 
 const useProgress = () => {
-  const [target, setTarget] = useState(100);
+  const [target, setTarget] = useState(0);
   const [progress, setProgress] = useState(0);
 
   const resetProgress = useCallback(() => {
     setProgress(0);
-    setTarget(100);
+    setTarget(0);
   }, []);
   const incrementProgress = useCallback(
     () => setProgress((prev) => prev + 1),
